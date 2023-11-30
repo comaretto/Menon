d7ec19e56c457ade09338fbb90bb84f79d97155f
comaretto <124884435+comaretto@users.noreply.github.com>
2023-11-30 11:46:16 -0300

    Add files via upload

---
+++ Menon2/index.js
@@ -0,0 +1,44 @@
+function SomMessi() {
+  document.querySelector("#som_tecla_Arvore").play();
+}
+document.querySelector("#tecla_Arvore").onclick = SomMessi;
+
+function SomCorrego() {
+  document.querySelector("#som_tecla_Corrego").play();
+}
+document.querySelector("#tecla_Corrego").onclick = SomCorrego;
+
+function SomDeserto() {
+  document.querySelector("#som_tecla_Deserto").play();
+}
+document.querySelector("#tecla_Deserto").onclick = SomDeserto;
+
+function SomFloresta() {
+  document.querySelector("#som_tecla_Floresta").play();
+}
+document.querySelector("#tecla_Floresta").onclick = SomFloresta;
+
+function SomFogueira() {
+  document.querySelector("#som_tecla_Fogueira").play();
+}
+document.querySelector("#tecla_Fogueira").onclick = SomFogueira;
+
+function SomMar() {
+  document.querySelector("#som_tecla_Mar").play();
+}
+document.querySelector("#tecla_Mar").onclick = SomMar;
+
+function SomPassaros() {
+  document.querySelector("#som_tecla_Passaros").play();
+}
+document.querySelector("#tecla_Passaros").onclick = SomPassaros;
+
+function SomRio() {
+  document.querySelector("#som_tecla_Rio").play();
+}
+document.querySelector("#tecla_Rio").onclick = SomRio;
+
+function SomTrovoada() {
+  document.querySelector("#som_tecla_Trovoada").play();
+}
+document.querySelector("#tecla_Trovoada").onclick = SomTrovoada;
