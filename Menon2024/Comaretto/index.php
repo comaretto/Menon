8187b9bed9f64544441d775caf7a7dd80f50920f
comaretto <124884435+comaretto@users.noreply.github.com>
2024-02-28 12:06:06 -0300

    Add files via upload

---
+++ Menon2024/Comaretto/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel</title>
+</head>
+<body>
+    <?php
+    $nome = "Gustavo";
+    const SOBRENOME = "Comaretto";
+    $peso=(int) 73;
+    const PESO = 73;
+    $altura = "1,83";
+    echo "meu nome é $nome " . SOBRENOME , "<br>";
+    echo "meu peso é $peso ","<br>";
+    echo "minha altura é $altura";
+    ?>
+
+    
+</body>
+</html>
